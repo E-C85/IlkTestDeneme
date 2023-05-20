9db5b04ee0031c146b43625b07a6fcbdf15e5141
Abdi <abdiarman2043@gmail.com>
2023-05-20 23:43:41 +0300

    case5 yapıldı

---
+++ src/test/java/Team_01/Test/AmazonTest_Abdi.java
@@ -0,0 +1,32 @@
+package Team_01.Test;
+
+import Team_01.PageClass.AmazonHomePage_Abdi;
+import Team_01.PageClass.AmazonResultPage_Abdi;
+import Team_01.Utilities.Driver;
+import org.testng.annotations.Test;
+
+public class AmazonTest_Abdi {
+
+
+
+
+    @Test
+public void testAmazon() throws InterruptedException {
+        Driver.getDriver().get("https://www.amazon.com/");
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().navigate().refresh();
+
+        AmazonHomePage_Abdi hp = new AmazonHomePage_Abdi();
+        hp.sarchProduct("Salesforce Apex Questions Bank");
+        Thread.sleep(1000);
+        AmazonResultPage_Abdi rp= new AmazonResultPage_Abdi();
+            System.out.println(rp.resultGetText());
+
+
+            Driver.getDriver().quit();
+
+
+    }
+
+
+}
